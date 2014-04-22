e59f2b6c2c815de771048af235424d13b1bdaaa9
Snowcat8436 <snowcat8436@gmail.com>
2014-04-22 23:41:07 +0900

    solve exercise #9

---
+++ 9.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+bool ispal(string str)
+{
+	string rstr = string(str.rbegin(), str.rend());
+	if (str == rstr)
+		return true;
+	else
+		return false;
+}
+
+int main()
+{
+	string str;
+	cin >> str;
+	int strlen = str.size();
+
+	for(int i=0; ; i++)	{
+		for(int j=0; j+strlen <= str.size(); j++) {
+			if(ispal(str.substr(j,strlen)))	{
+				cout << str.substr(j,strlen);
+				return 0;
+			}
+		}
+
+		if(strlen != 1) {
+			strlen -= 1;
+			i=-1;
+			continue;
+		}
+		else
+			break;
+	}
+	cout << "";
+	return 0;
+}
