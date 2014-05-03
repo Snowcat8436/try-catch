b6b4e708cdd847ae5967579be13f2888f3c84ec4
Snowcat8436 <snowcat8436@gmail.com>
2014-05-04 06:20:22 +0900

    solve exercise #10

---
+++ 10.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+	int horses,seek;
+	cin >> horses;
+	cin >> seek;
+
+	cout << ceil(horses / 9);
+	// Only this?
+
+	return 0;
+}
